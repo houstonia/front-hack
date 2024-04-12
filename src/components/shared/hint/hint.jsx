7d21def979fba60c89f109f1dd819c05be333590
houstonia~ <uwuowo733@gmail.com>
2024-04-13 02:06:24 +0300

    add

--- src/components/shared/hint/hint.jsx
+++ src/components/shared/hint/hint.jsx
@@ -1,14 +1,13 @@
-
 import BotImage from "@/assets/bot-image.svg";
 import AiBot from "@/assets/icons/aibot.svg"
 import Glow from "@/assets/icons/glow.svg"
 
 export const Hint = () => {
     return (
-        <div className="absolute flex items-center top-0 left-[400px]">
-              <img src={Glow} className="absolute"/>
-            <img src={AiBot}/>
-            <div className="text-white w-[193px] h-[60px] px-3.5 py-2 bg-slate-600 rounded-xl ml-[-200px]">Здесь ты можешь найти меня!</div>
+        <div className="absolute flex items-center top-[] left-[400px]">
+            <img src={AiBot} />
+            <img src={Glow} className="absolute" />
+            <div className="text-white w-[193px] h-[60px] px-3.5 py-2 bg-slate-600 rounded-xl ml-[-60px]">Здесь ты можешь найти меня!</div>
         </div>
     )
 }
